838e9f06247ea2ad5acbeb867b8c94f3c39b4614
mariodac <mdac.mario@gmail.com>
2019-12-05 17:10:23 -0300

    Ajustes finais

--- _main.py
+++ _main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     # print(resultado)
     print('Foram encontrados: {} documentos.'.format(len(resultado)))
     # ordenando os documentos
-    print("Documentos por ordem decrescente de relevância:")
+    print("Documentos por ordem decrescente de similiridade:")
     docs_ordenados = sorted(resultado, key = resultado.get, reverse = True)
     for item in range(0, len(docs_ordenados)):
         if item == 0:
